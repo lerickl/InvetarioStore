47a850ade533f3f218e2a2b763564078d8ba34db
lerickl <nearerick@gmail.com>
2024-01-24 15:16:51 -0500

    second  commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devServer: {
-    allowedHosts: ['localhost', '491g4fhf-3000.brs.devtunnels.ms'],
-  },
+ 
 }
 
 module.exports = nextConfig
