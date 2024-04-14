ca6110c76056ca9ad1b7649335077fc78107e2e9
lerickl <nearerick@gmail.com>
2024-04-14 04:52:49 -0500

    new Version_1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './ui/styles/globals.css'
 import { Provider } from './ui/ThemeChanger/ThemeProvider' 
 const inter = Inter({ subsets: ['latin'] })
-
+export const runtime = "edge";
 export const metadata: Metadata = {
   title: 'Dulce como tu',
   description: 'control de inventario y ventas ',
@@ -14,6 +14,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="es" suppressHydrationWarning>
       <body>
